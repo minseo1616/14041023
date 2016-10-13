c0ca60d6928e913dbd69071d6c811d01bc7b462b
'^' <'^'@DESKTOP-5DHRMTI>
2016-10-13 14:57:00 +0900

    remote repository add a push pushprint.java

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -5,11 +5,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
-<<<<<<< HEAD
 		System.out.println("Earth's End Dancehall");
-=======
 		System.out.println("Hello Dance World");
->>>>>>> refs/heads/hotfix
 
 	}
 
